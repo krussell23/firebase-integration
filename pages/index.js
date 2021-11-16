a65bb9e8948650644ad1c26fe750107bb8f8cd05
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2021-11-16 09:16:23 -0700

    good to go

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import firebase from "./../firebase"
+
 import Brand from "../components/brand"
 import NavBar from "../components/navbar"
 
 
 function Home() {
+  console.log(firebase)
   return (
     <>
       <NavBar/>
