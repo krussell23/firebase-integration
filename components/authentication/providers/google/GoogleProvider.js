8dd4d907d684e75f9234caa1941cb2fdfc65a545
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2021-11-30 10:35:01 -0700

    google login

--- components/authentication/providers/google/GoogleProvider.js
+++ components/authentication/providers/google/GoogleProvider.js
@@ -1,12 +1,32 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
+import { useState } from 'react'
+import { useAuth } from 'lib/hooks/useAuth'
+import { auth } from 'lib/firebase';
 import { ProviderButton } from "ui/buttons";
 import google from "./google.png";
 
-function GoogleProvider({ children,  ...props }) {
- 
+function GoogleProvider({ children, ...props }) {
+  const router = useRouter()
+  const user = useAuth()
+  const [isValidUser, setIsValidUser] = useState(null)
+  const provider = new GoogleAuthProvider()
+
+  async function requestLogin() {
+    setIsValidUser(await signInWithPopup(auth, provider))
+  }
+  function handleClicl() {
+    //sign in with google
+    requestLogin()
+  }
+
+  if (isValidUser) {
+    router.push('/todo')
+  }
 
   return (
-    <ProviderButton >
+    <ProviderButton onClick={handleClick} {...props} >
       <div>
         <Image
           src={google}
